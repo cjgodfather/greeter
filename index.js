317005dab3b12e0c55c5456456385c2dc3b2656c
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2019-10-31 13:41:39 -0400

    changed

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ server.get("/", (req, res) => {
   res.status(200).json({ hello: "Web 23" });
 });
 
+server.get("api/", (req, res) => {
+  res.status(200).json({ hello: "this is api" });
+});
+
 const port = process.env.PORT || 4000;
 
 console.log(process.env.PORT);
